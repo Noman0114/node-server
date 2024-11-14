531213ef7436d7f8ef08247e7f51bbbe4aa74c4a
Noman0114 <na94593170@gmail.com>
2024-11-14 10:54:44 -0800

    link local

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: ['http://localhost:3000', 'https://myreeldream-app.vercel.app'],
+    origin: 'http://localhost:3000',
     // Allow requests from your Next.js frontend
     methods: ['GET', 'POST'], // Allow specific methods
     allowedHeaders: ['Content-Type'], // Specify allowed headers if needed
